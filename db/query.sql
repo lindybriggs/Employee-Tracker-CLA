8e4d1e0d8c948bef56a3d64c2d54768a0d96c6b2
Lindy Briggs <briggs.lindy@yahoo.com>
2022-05-02 15:22:39 -0500

    Updated content of db files.

--- db/query.sql
+++ db/query.sql
@@ -1,3 +1,10 @@
-SELECT employee.first_name, employee.last_name, role.title FROM role
-JOIN employee
-ON role.id = employee.role_id;
+select employee.id, employee.first_name, employee.last_name, role.title as roleTitle, department.name as departmentName, role.salary, manager.first_name AS managerName
+    from (
+    (employee INNER JOIN role ON role_id = role.id)
+    inner join 
+    (select * from employee) as manager
+    on manager.id = employee.manager_id
+    INNER JOIN 
+    department 
+    ON department_id = department.id
+    );
